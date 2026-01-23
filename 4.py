1dfdbfc33364f7870e206607724679d6d02034fa
Chitranjan Gupta <gchitranjan82@gmail.com>
2026-01-24 01:13:13 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,13 @@
+a = 23
+b = 1.34
+c = "Hello"
+d = (1,2,3)
+e = (4,5,6)
+f = True
+
+print("Integer:", a)
+print("Float:", b)
+print("string:", c)
+print("list:", d)
+print("tumple:", e)
+print("Boolean:", f)
